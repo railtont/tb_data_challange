9e91eb676900617ae7901cf03860ee3aec8833d6
railton.calheiros@blueshift.com.br <railton.calheiros@blueshift.com.br>
2021-09-08 19:06:57 -0300

    updated connetion files

--- connections/postgres.py
+++ connections/postgres.py
@@ -1,4 +1,5 @@
 import psycopg2
 
-def get_pg_conn():
-    pass
+def get_pg_conn(dbname="redshiftdb", user="redshift", host="host", port="1234", password="pwd"):
+    conn = psycopg2.connect(dbname=dbname, user=user, host=host, password=password, port=port)
+    return conn
--- connections/redshift.py
+++ connections/redshift.py
@@ -1,18 +1,5 @@
 import psycopg2
-#Amazon Redshift connect string 
-conn_string = "dbname='***' port='5439' user='***' password='***' host='mycluster.***.redshift.amazonaws.com'"  
-#connect to Redshift (database should be open to the world)
-con = psycopg2.connect(conn_string);
-sql="""COPY %s FROM '%s' credentials 
-      'aws_access_key_id=%s; aws_secret_access_key=%s'
-       delimiter '%s' FORMAT CSV %s %s; commit;""" % 
-      (to_table, fn, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY,delim,quote,gzip)
 
-cur = con.cursor()
-cur.execute(sql)
-con.close() 
-
-def get_rs_conn():
-    pass
-
-# https://aws.amazon.com/pt/blogs/big-data/using-the-amazon-redshift-data-api-to-interact-with-amazon-redshift-clusters/
+def get_rs_conn(dbname="redshiftdb", user="redshift", host="host", port="1234", password="pwd"):
+    conn = psycopg2.connect(dbname=dbname, user=user, host=host, password=password, port=port)
+    return conn
