47dda84063ba3b85515a7ec5df7f651e318823bc
railtont <railton.thales@hotmail.com>
2021-09-08 19:08:00 -0300

    Update postgres.py

--- connections/postgres.py
+++ connections/postgres.py
@@ -1,5 +1,5 @@
 import psycopg2
 
-def get_pg_conn(dbname="redshiftdb", user="redshift", host="host", port="1234", password="pwd"):
+def get_pg_conn(dbname="postgresdb", user="postgres", host="host", port="1234", password="pwd"):
     conn = psycopg2.connect(dbname=dbname, user=user, host=host, password=password, port=port)
     return conn
